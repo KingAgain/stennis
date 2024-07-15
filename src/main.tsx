09c6da11905dfe324715d5a85aca9df7fc2af940
KingAgain <97130830+KingAgain@users.noreply.github.com>
2024-07-15 11:55:16 +0800

    chore: Update dependencies and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index'
+import './index.css'
 
 document.title = 'STennis'
 const rootElement = document.getElementById('root');
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,32 +3,35 @@ import React from 'react';
 import "./home.css"
 import { Carousel } from 'antd';
 const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-    margin: '0',
-  } as React.CSSProperties;
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+  margin: '0',
+} as React.CSSProperties;
 
 const PHome: React.FC = () => {
   return (
     <div className="home-page" >
-        <Carousel autoplay style={{maxWidth:'100vh'}}>
-            <div>
-            <h3 style={contentStyle}>1</h3>
-            </div>
-            <div>
-            <h3 style={contentStyle}>2</h3>
-            </div>
-            <div>
-            <h3 style={contentStyle}>3</h3>
-            </div>
-            <div>
-            <h3 style={contentStyle}>4</h3>
-            </div>
-    </Carousel>
-  </div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Carousel autoplay style={{ maxWidth: '100vh' }}>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </div>
   );
 };
 
